31a98772be5a6611a648ae9be16e14dc026addb8
Francesco Montanaro <francesco.montanaro998@gmail.com>
2025-12-20 15:26:24 +0100

    Removed useless CategoricalCrossEntropy loss

--- Deep Learning/src/loss_functions/__init__.py
+++ Deep Learning/src/loss_functions/__init__.py
@@ -1,6 +1,5 @@
 from .base import LossFn
 
 from .binary_cross_entropy import BinaryCrossEntropy
-from .categorical_cross_entropy import CategoricalCrossEntropy
 from .cross_entropy import CrossEntropy
 from .mean_square_error import MeanSquareError
